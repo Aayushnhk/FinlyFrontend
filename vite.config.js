93b702d4844e6b0a65cd539ba7b101e275fb307c
Aayush Sharma <139450872+Aayushnhk@users.noreply.github.com>
2025-04-23 00:40:13 +0530

    vite configuration changed

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:3000',
+      '/api': {
+        target: 'https://finiybackend-production.up.railway.app',
+        changeOrigin: true,
+        secure: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
 });
